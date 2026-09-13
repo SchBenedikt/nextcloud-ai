e0c02fe3ee05a90314ab0f2e2507a209cd0f5bb4
Benedikt Schächner <sch.benedikt@proton.me>
2026-09-13 15:34:00 +0000

    perf: trim indexer allocator caches after runs

--- lib/Service/Indexer.php
+++ lib/Service/Indexer.php
@@ -478,6 +478,12 @@ class Indexer {
             } finally {
                 $this->scheduler->releaseSlot($userId);
                 $this->lockingProvider->releaseLock($lockPath, ILockingProvider::LOCK_EXCLUSIVE);
+                if (function_exists('gc_collect_cycles')) {
+                    gc_collect_cycles();
+                }
+                if (function_exists('gc_mem_caches')) {
+                    gc_mem_caches();
+                }
             }
         }
 
